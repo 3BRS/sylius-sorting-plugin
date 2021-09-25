febeb3bd2d0ad8ebe9a995273f092ed507c57a0d
Ondřej Kühnel <ondrej@3brs.com>
2021-09-25 22:51:23 +0200

    lint

--- bin/symfony-lint.sh
+++ bin/symfony-lint.sh
@@ -5,4 +5,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 bin/console --no-interaction lint:yaml src
 bin/console --no-interaction lint:container
-bin/console --no-interaction src/Resources/views --show-deprecations
+bin/console --no-interaction lint:twig src --show-deprecations
