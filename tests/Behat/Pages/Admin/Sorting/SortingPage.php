7e12d6c282a0293f6d2e425bd5e8089840706460
Ondřej Kühnel <bibrak.ondra@gmail.com>
2021-09-27 09:15:05 +0200

    Symfony52
    Symfony 5.2

--- tests/Behat/Pages/Admin/Sorting/SortingPage.php
+++ tests/Behat/Pages/Admin/Sorting/SortingPage.php
@@ -8,38 +8,38 @@ use FriendsOfBehat\PageObjectExtension\Page\SymfonyPage;
 
 final class SortingPage extends SymfonyPage implements SortingPageInterface
 {
-	public function getRouteName(): string
-	{
-		return 'threebrs_admin_sorting_products';
-	}
+    public function getRouteName(): string
+    {
+        return 'threebrs_admin_sorting_products';
+    }
 
-	public function saveSorting(): void
-	{
-		$this->getElement('save_sorting')->click();
-	}
+    public function saveSorting(): void
+    {
+        $this->getElement('save_sorting')->click();
+    }
 
-	protected function getDefinedElements(): array
-	{
-		return array_merge(parent::getDefinedElements(), [
-			'save_sorting' => 'button:contains("Save positions")',
-		]);
-	}
+    protected function getDefinedElements(): array
+    {
+        return array_merge(parent::getDefinedElements(), [
+            'save_sorting' => 'button:contains("Save positions")',
+        ]);
+    }
 
-	public function getPosition($arg1, int $int): void
-	{
-		$Page = $this->getSession()->getPage();
-		$firstE = $Page->find('css', '#sortableProducts .sortableItem:nth-child(' . $int . ') .content .header')->getText();
-		if ($firstE != $arg1) {
-			throw new \RuntimeException(sprintf($firstE));
-		}
-	}
+    public function getPosition($arg1, int $int): void
+    {
+        $Page = $this->getSession()->getPage();
+        $firstE = $Page->find('css', '#sortableProducts .sortableItem:nth-child(' . $int . ') .content .header')->getText();
+        if ($firstE != $arg1) {
+            throw new \RuntimeException(sprintf($firstE));
+        }
+    }
 
-	public function getState($arg1): void
-	{
-		$Page = $this->getSession()->getPage();
-		$elementA = $Page->find('css', '#sortableProducts .sortableItem:nth-child(1)')->getHtml();
-		if (strpos($elementA, 'style="opacity: 0.5') == false) {
-			throw new \RuntimeException(sprintf($elementA));
-		}
-	}
+    public function getState($arg1): void
+    {
+        $Page = $this->getSession()->getPage();
+        $elementA = $Page->find('css', '#sortableProducts .sortableItem:nth-child(1)')->getHtml();
+        if (strpos($elementA, 'style="opacity: 0.5') == false) {
+            throw new \RuntimeException(sprintf($elementA));
+        }
+    }
 }
